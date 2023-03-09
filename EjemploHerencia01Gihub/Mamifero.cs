05ee92e04f0306604ae4916debebd9139f07d1d7
PacoBarbaAlvarez <pacobarbitaalvarez@gmail.com>
2023-03-09 12:13:34 +0100

    Creamos de nuevo el metodo TipoAlimentacion con el override delante para poder sobreescribir el metodo de la clase animal, asignandole asi un nuevo valor sin necesidad de crear otro metodo distinto.

--- EjemploHerencia01Gihub/Mamifero.cs
+++ EjemploHerencia01Gihub/Mamifero.cs
@@ -8,5 +8,17 @@ namespace EjemploHerencia01Gihub
 {
     internal class Mamifero : Animal
     {
+        public override string TipoAlimentacion()
+        {
+            string cadena;
+
+            _alimentacion = "";
+
+            cadena = base.TipoAlimentacion();
+
+            cadena += ": Otros seres vivos";
+
+            return cadena;
+        }
     }
 }
