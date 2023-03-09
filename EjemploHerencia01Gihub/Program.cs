37195c803e3a0c357507a930bdc8a7819d5c53f0
PacoBarbaAlvarez <pacobarbitaalvarez@gmail.com>
2023-03-09 12:07:16 +0100

    Agregamos los atributos de la clases y los inicializamos

--- EjemploHerencia01Gihub/Program.cs
+++ EjemploHerencia01Gihub/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            Animal animal1;
+            Mamifero mamifero1;
+            Aves pajaro;
+
+            animal1 = new Animal();
+            mamifero1 = new Mamifero();
+            pajaro = new Aves();
         }
     }
 }
