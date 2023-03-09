48355ab73f24e3d2751752c764d8d3bd3743ac1f
PacoBarbaAlvarez <pacobarbitaalvarez@gmail.com>
2023-03-09 12:08:05 +0100

    Creamos las constantes y las variables necesarias y las inicializamos

--- EjemploHerencia01Gihub/Animal.cs
+++ EjemploHerencia01Gihub/Animal.cs
@@ -8,5 +8,9 @@ namespace EjemploHerencia01Gihub
 {
     internal class Animal
     {
+        private const int TAMANIO = 20;
+
+        protected string _alimentacion = "Comida";
+        private string _nombre;
     }
 }
